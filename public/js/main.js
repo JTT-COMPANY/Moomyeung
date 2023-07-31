a79d23c15f11ff5fb2401f58d53cabe212ea001f
hapl197 <neix604@gmail.com>
2023-07-31 16:53:52 +0900

    20230731 컨텐츠 전환 애니메이션 추가, 컨텐츠 레이아웃 설정

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,26 @@
-console.log('테스트');
+const article = document.querySelectorAll('article');
+const goNext = document.querySelector('#goNext');
+const classArr = ['firstBox', 'secondBox', 'thirdBox'];
+
+let i = 0;
+
+for (let a of article) {
+  a.classList.add(classArr[i]);
+  i++;
+}
+
+// 버튼 클릭 시 클래스 토글
+goNext.addEventListener('click', function() {
+  for (let a of article) {
+    if (a.classList.contains(classArr[0])) {
+      a.classList.remove(classArr[0])
+      a.classList.add(classArr[2])
+    } else if (a.classList.contains(classArr[1])) {      
+      a.classList.remove(classArr[1])
+      a.classList.add(classArr[0])
+    } else if (a.classList.contains(classArr[2])) {
+      a.classList.remove(classArr[2])
+      a.classList.add(classArr[1])
+    }
+  }
+});
